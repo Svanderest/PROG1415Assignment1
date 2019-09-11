50c0fedb72d60a9306b5a0db6726e853aa209442
Svanderest <35896486+Svanderest@users.noreply.github.com>
2019-09-11 12:39:07 -0400

    fix errors

--- Assignment1/src/prog1415/TestProgram.java
+++ Assignment1/src/prog1415/TestProgram.java
@@ -12,38 +12,36 @@ public class TestProgram {
 		save.makeDeposit(300);
 		save.applyInterest();
 		save.makeWithdrawal(25);
-		JOptionPane.showMessageDialog(null, save.toString());
+		JOptionPane.showMessageDialog(null, save, save.getBankName(), 1);		
 		
 		ChequingAccount ch = new ChequingAccount(5555, new Date());
 		ch.setFee(5.55);
 		ch.makeDeposit(100);
 		ch.applyFee();
 		ch.makeWithdrawal(350);
-		JOptionPane.showMessageDialog(null, ch.toString());
+		JOptionPane.showMessageDialog(null, ch, ch.getBankName(), 0);		
 		
 		//test SuperChequingAccount
-		SuperChequingAccount super = new SuperChequingAccount(5555,new Date());
-		super.setFee(3.25);
-		super.setOverDraft(200);
-		super.makeDeposit(200);
-		super.applyFee();
-		super.makeWithdrawal(350);
-		JOptionPane.showMessageDialog(null, super.toString());
+		SuperChequingAccount sup = new SuperChequingAccount(5555,new Date());
+		sup.setFee(3.25);
+		sup.setOverDraft(200);
+		sup.makeDeposit(200);
+		sup.applyFee();
+		sup.makeWithdrawal(350);
+		JOptionPane.showMessageDialog(null, sup, sup.getBankName(), 2);
 
-		//Polymorphic programming - use a base class reference
 		BankAccount b = new ChequingAccount(7777,new Date());
 		b.makeDeposit(100);
 		b.makeWithdrawal(50);
-		JOptionPane.showMessageDialog(null, b.toString());
-
-		//Polymorphic programming - use a base class array to
+		JOptionPane.showMessageDialog(null, b, b.getBankName(), 3);
+		
 		//store and retrieve any derived bank account type
 		BankAccount accounts[] = new BankAccount[3];
 		accounts[0] = new ChequingAccount(1234,new Date());
 		accounts[1] = new SuperChequingAccount(5678,new Date());
 		accounts[2] = new SavingsAccount(9876,new Date());
-		for(int i = 0; i < accounts.Length; i++)		
-			JOptionPane.showMessageDialog(null, accounts[0].toString());
+		for(int i = 0; i < accounts.length; i++)		
+			JOptionPane.showMessageDialog(null, accounts[i], accounts[i].getBankName(),i);
 			
 		if(accounts[0] instanceof ChequingAccount)
 		{
@@ -51,7 +49,7 @@ public class TestProgram {
 			ChequingAccount temp = (ChequingAccount) accounts[0];
 			//access specific derived class members
 			temp.applyFee();
-			JOptionPane.showMessageDialog(null, temp.toString());
+			JOptionPane.showMessageDialog(null, temp, temp.getBankName(),1);
 		}
 
 	}
