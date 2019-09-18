2ea170832ee3811ae33d077a16fb24bfa4c2d269
Svanderest <35896486+Svanderest@users.noreply.github.com>
2019-09-18 10:47:01 -0400

    added splash screen

--- Assignment1/src/prog1415/Splash.java
+++ Assignment1/src/prog1415/Splash.java
@@ -1,5 +1,6 @@
 package prog1415;
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
 
@@ -7,5 +8,23 @@ public class Splash extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 9130785213335296281L;	
+	private Thread t;
+	
+	public Splash()
+	{
+		this.setBounds(100, 100, 500, 400);
+		this.getContentPane().setBackgroundColor(COLOR.BLACK);
+		this.setUndecorated();
+		t = new Thread(new Runnable(){
+			public void run()
+			{
+				Thread.sleep(5000);
+				new ATM();
+				this.dispose();
+			}
+		});
+		this.setVisible(true);
+		t.start();
+	}
 
 }
