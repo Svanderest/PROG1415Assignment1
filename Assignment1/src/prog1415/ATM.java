ce0a6712edf6e60fb8c5416219e70e594d9e0059
Svanderest <35896486+Svanderest@users.noreply.github.com>
2019-09-26 12:31:17 -0400

    finished assignment

--- Assignment1/src/prog1415/ATM.java
+++ Assignment1/src/prog1415/ATM.java
@@ -3,14 +3,20 @@ package prog1415;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
 
 import javax.swing.*;
 
 public class ATM extends JFrame {
 	
 	private static final long serialVersionUID = 4517103375856486687L;
-	JButton btnExit = new JButton("Exit"); 	
+	BankAccount currentAccount;
+	ArrayList<BankAccount> bankAccounts;
 	
+	JButton btnExit = new JButton("Exit"); 		
 	JPanel south = new JPanel(new BorderLayout()); 	
 	JPanel east = new JPanel(new FlowLayout(FlowLayout.CENTER));
 	JPanel center = new JPanel(new GridLayout(5, 1));
@@ -35,11 +41,18 @@ public class ATM extends JFrame {
 	JButton btnDeposit = new JButton("Deposit");
 	
 	public ATM() {
+		bankAccounts = new ArrayList<BankAccount>();	
+		bankAccounts.add(new ChequingAccount(0, new Date()));
+		bankAccounts.add(new SavingsAccount(1, new Date()));
+		bankAccounts.add(new SuperChequingAccount(2, new Date()));
 		int heightOfBtn = (int)btnExit.getPreferredSize().getHeight();
 		this.setTitle("My Atm"); 
 		this.setBounds(100, 100, 400, 400);
 		
 		btnExit.addActionListener(new Exit());		
+		btnLogin.addActionListener(new Login());
+		btnDeposit.addActionListener(new Deposit());
+		btnWithdraw.addActionListener(new Withdraw());
 	
 		this.getContentPane().add(south, BorderLayout.SOUTH); 
 		this.getContentPane().add(center, BorderLayout.CENTER);
@@ -63,7 +76,8 @@ public class ATM extends JFrame {
 		action.add(actionEast, BorderLayout.EAST);
 		action.add(actionCenter, BorderLayout.CENTER);
 		action.add(actionWest, BorderLayout.WEST);
-		txtAction.setPreferredSize(new Dimension(250,heightOfBtn));
+	
+		txtAction.setPreferredSize(new Dimension(180,heightOfBtn));
 		actionCenter.add(txtAction);
 		actionWest.add(btnWithdraw);
 		actionEast.add(btnDeposit);
@@ -74,7 +88,6 @@ public class ATM extends JFrame {
 	
 	class Exit implements ActionListener
 	{
-
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
@@ -89,6 +102,34 @@ public class ATM extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
+			String accountNo = ATM.this.txtAccountNo.getText();
+			try
+			{
+				int acNo = Integer.parseInt(accountNo);
+				ATM.this.currentAccount = null;
+				for(int i = 0; i < ATM.this.bankAccounts.size(); i++)
+				{
+					if(ATM.this.bankAccounts.get(i).getAccountNumber() == acNo)
+					{
+						ATM.this.currentAccount = ATM.this.bankAccounts.get(i);
+						break;
+					}
+				}
+				if(ATM.this.currentAccount == null)
+				{
+					JOptionPane.showMessageDialog(null, "Account not found", "Invalid input",1);
+					ATM.this.lblAmount.setText("");
+				}
+				else
+				{
+					ATM.this.lblAmount.setText(Double.toString(ATM.this.currentAccount.getBalance()));
+				}
+			}
+			catch(NumberFormatException e)
+			{
+				JOptionPane.showMessageDialog(null, "Please enter a numeric value", "Invalid input",0);
+				ATM.this.lblAmount.setText("");
+			}			
 		}
 	}
 
@@ -97,14 +138,48 @@ public class ATM extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
+			if(ATM.this.currentAccount == null)
+				JOptionPane.showMessageDialog(null, "Please login", "Login required",1);
+			else
+			{
+				try
+				{
+					double amount = Double.parseDouble(ATM.this.txtAction.getText());
+					ATM.this.currentAccount.makeDeposit(amount);
+					ATM.this.lblAmount.setText(Double.toString(ATM.this.currentAccount.getBalance()));
+				}
+				catch(NumberFormatException e)
+				{
+					JOptionPane.showMessageDialog(null, "Please enter a numeric value", "Invalid input",0);					
+				}
+			}
+			ATM.this.txtAction.setText("");
 		}
 	}
 
 	class Withdraw implements ActionListener
-	{
+	{		
+		// TODO Auto-generated method stub
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
+			if(ATM.this.currentAccount == null)
+				JOptionPane.showMessageDialog(null, "Please login", "Login required",1);
+			else
+			{
+				try
+				{
+					double amount = Double.parseDouble(ATM.this.txtAction.getText());
+					ATM.this.currentAccount.makeWithdrawal(amount);
+					ATM.this.lblAmount.setText(Double.toString(ATM.this.currentAccount.getBalance()));
+				}
+				catch(NumberFormatException e)
+				{
+					JOptionPane.showMessageDialog(null, "Please enter a numeric value", "Invalid input",0);					
+				}
+			}
+			ATM.this.txtAction.setText("");
 		}
-	}	
-}
+	}
+}	
+
